999bb9f7e895810a066f5cd8c48dfdc5e19e7514
zengge96 <zengge99@gmail.com>
2026-04-07 14:32:20 +0800

    ok

---
+++ scripts/get_one_bach_lines.js
@@ -0,0 +1,7 @@
+// todo
+// 输入1：带行号的文件（/tmp/incomplete_lines/incomplete_with_num.txt）
+// 输入2：批次编号（例如第1批，第2批）
+// 输入3：每批次行数（例如100）
+// 输出：带原始行号的文件（/tmp/incomplete_lines/ibatch1_names.txt）
+
+//作用：从incomplete_with_num.txt指定批次的所有行。
