51da16e77ebe4e90827c0bdc3927fd8d47ad9190
zengge96 <zengge99@gmail.com>
2026-04-09 09:00:37 +0800

    ok

--- scripts/gen_file_with_num.js
+++ scripts/gen_file_with_num.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 /**
  * 步骤1：生成行号文件，将原始的文件加上行号
- * 输入: 输入文件
- * 输出: 日志打印方式输出，格式：行号#路径（行号是原始文件中的行号）
+ * 输入: 输入文件, 输出文件
+ * 输出: 行号#路径（行号是原始文件中的行号）
  * 
  * 用法: node gen_file_with_num.js <输入文件> <输出文件>
  */
 const fs = require('fs');
 
-if (process.argv.length < 3) {
-  console.log('用法: node gen_file_with_num.js <输入文件>');
+if (process.argv.length < 4) {
+  console.log('用法: node gen_file_with_num.js <输入文件> <输出文件>');
   process.exit(1);
 }
 
 const INPUT_FILE = process.argv[2];
-//const OUTPUT_FILE = process.argv[3];
+const OUTPUT_FILE = process.argv[3];
 
 function main() {
-  //console.log('【步骤1】生成行号文件\n');
-  //console.log(`输入: ${INPUT_FILE}`);
-  //console.log(`输出: ${OUTPUT_FILE}`);
+  console.log('【步骤1】生成行号文件\n');
+  console.log(`输入: ${INPUT_FILE}`);
+  console.log(`输出: ${OUTPUT_FILE}`);
 
   // 读取原始文件
   const allLines = fs.readFileSync(INPUT_FILE, 'utf-8')
@@ -34,11 +34,10 @@ function main() {
   }
 
   // 写入输出
-  //fs.writeFileSync(OUTPUT_FILE, output);
-  //console.log(`完成！已保存到: ${OUTPUT_FILE}\n`);
-  //console.log('示例:');
-  //output.split('\n').slice(0, 3).forEach(l => console.log(l));
-  output.split('\n').forEach(l => console.log(l));
+  fs.writeFileSync(OUTPUT_FILE, output);
+  console.log(`完成！已保存到: ${OUTPUT_FILE}\n`);
+  console.log('示例:');
+  output.split('\n').slice(0, 3).forEach(l => console.log(l));
 }
 
 main();
